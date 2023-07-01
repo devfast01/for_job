0e1e1ac810a98baa05f7a79bf36c62bfbb3c8bc9
devfast01 <devfast01@github.com>
2023-07-01 21:23:10 +0500

    Ecommerse

---
+++ app/src/main/java/com/example/for_job/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.for_job
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
